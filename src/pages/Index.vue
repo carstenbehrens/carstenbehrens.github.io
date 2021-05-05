37d6f1c6e1f595fbafaf674f1c9ee36a302951ac
Carsten Behrens <behrens.carsten@guj.de>
2021-05-05 07:35:47 +0200

    Fix styling on mobile

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -17,24 +17,26 @@
         <div
           class="flex flex-col justify-between my-16 items-start justify-items-center"
         >
-          <div class="self-start flex flex-col items-center sm:flex-row pb-8">
-            <div class="ml-6 pb-8 sm:pb-0">
+          <div class="flex flex-col items-center sm:flex-row pb-8 self-stretch sm:self-auto">
+            <div class="sm:ml-6 pb-8 sm:pb-0">
               <g-image
                 src="../../static/avatar_new.jpg"
                 alt="avatar"
                 class="w-32 h-32 rounded-full"
               />
             </div>
-            <h1 class="sm:ml-8 text-2xl font-bold ml-6">
+            <h1 class="sm:ml-8 text-2xl font-bold sm:ml-6">
               Hi 👋, I'm Carsten Behrens
             </h1>
           </div>
-          <p class="flex-1 text-lg sm:text-xl ml-6 pb-8">
+          <p class="flex-1 text-lg sm:text-xl sm:ml-6 pb-8">
             I help people create websites and web-applications. On this website
             I publish articles mostly focused on web development.
           </p>
-          <p class="flex-1 text-lg sm:text-xl ml-6 font-bold">
-            You can reach out to me using my email address:
+          <p class="flex-1 text-lg sm:text-xl sm:ml-6 font-bold">
+            You can reach out to me using my email:
+          </p>
+          <p class="flex-1 text-lg sm:text-xl sm:ml-6 font-bold">
             carsten.behrens1@gmail.com
           </p>
         </div>
