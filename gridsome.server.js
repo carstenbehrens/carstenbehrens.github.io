5bba87c436a51e3a2b6082e41052ab93f9bcb637
carstenbehrens <carsten.behrens1@googlemail.com>
2021-06-30 21:03:07 +0200

    feat: create searchData during build process

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,11 +1,39 @@
 // Server API makes it possible to hook into various parts of Gridsome
 // on server-side and add custom data to the GraphQL data layer.
 // Learn more: https://gridsome.org/docs/server-api
+const fs = require("fs");
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+module.exports = function(api, options) {
+  const searchData = {
+    posts: [],
+  };
 
-module.exports = function (api, options) {
-  api.loadSource(store => {
-  })
-}
+  api.loadSource((store) => {});
+
+  api.onCreateNode((options) => {
+    if (options.internal.typeName === "Post") {
+      const { title, summary, tags } = options;
+      const post = {
+        title,
+        summary,
+        tags,
+      };
+      searchData.posts.push(post);
+    }
+  });
+
+  api.afterBuild(() => {
+    fs.writeFile(
+      "./static/searchData.json",
+      JSON.stringify(searchData),
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
+  });
+};
